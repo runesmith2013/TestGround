d42d182bf2e10778daccebd4855d0ba7d48d34f4
runesmith2013 <robertbarr.uk@gmail.com>
2013-12-08 18:20:53 +0000

    Initial commit

---
+++ HelloGit/src/com/rob/test/hello/HelloWorld.java
@@ -0,0 +1,13 @@
+package com.rob.test.hello;
+
+/**
+ * Created by rob on 08/12/13.
+ */
+public class HelloWorld {
+
+    public static void main(String[]args) {
+
+        System.out.println("Hello World!");
+
+    }
+   }
